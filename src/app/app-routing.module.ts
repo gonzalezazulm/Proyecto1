e608329924eb1b7b617b42bef128bdf1737d8a69
gonzalezazulm <azulgonzzalez@gmail.com>
2022-07-07 10:47:38 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,14 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { FacebookComponent } from './pages/facebook/facebook.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProgramacionComponent } from './pages/programacion/programacion.component';
-import { RadioComponent } from './pages/radio/radio.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { TeamComponent } from './pages/team/team.component';
 import { YoutubeComponent } from './pages/youtube/youtube.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'', component:HomeComponent},
   {path:'about', component: AboutComponent},
-  {path:'team', component: TeamComponent},
-  {path:'services', component: ServicesComponent},
   {path:'contact', component: ContactComponent},
   {path:'programacion', component: ProgramacionComponent},
-  {path:'radio', component: RadioComponent},
   {path:'facebook', component:FacebookComponent},
   {path:'youtube', component:YoutubeComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,11 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { TeamComponent } from './pages/team/team.component';
-import { ServicesComponent } from './pages/services/services.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ProgramacionComponent } from './pages/programacion/programacion.component';
-import { RadioComponent } from './pages/radio/radio.component';
 import { FacebookComponent } from './pages/facebook/facebook.component';
 import { YoutubeComponent } from './pages/youtube/youtube.component';
 
@@ -23,14 +20,11 @@ import { YoutubeComponent } from './pages/youtube/youtube.component';
     MenuComponent,
     HomeComponent,
     ContactComponent,
-    TeamComponent,
-    ServicesComponent,
     AboutComponent,
     NotpagefoundComponent,
     FooterComponent,
     HeaderComponent,
     ProgramacionComponent,
-    RadioComponent,
     FacebookComponent,
     YoutubeComponent
   ],
